955231fe51ac2c883fc1b7fb0d8c62ae72f31d9e
Youngeun Jun <121297085+Junyewdd@users.noreply.github.com>
2024-02-06 02:19:51 +0900

    [Silver I] Title: 카드 구매하기, Time: 0 ms, Memory: 2064 KB -BaekjoonHub

---
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int dp[10001], card[1001];
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int n;
+	cin >> n;
+	card[0] = 0;
+	for (int i = 1; i <= n; i++) {
+		cin >> card[i];
+		int max = 0, sum = 0;
+		for (int j = 1; j <= (i / 2 + 1); j++) {
+			sum = card[j] + card[i - j];
+			if (sum > max)max = sum;
+		}
+		if(max>card[i])card[i] = max;
+	}
+	cout << card[n];
+}
